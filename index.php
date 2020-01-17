ab9309e97b20e15dfeb549d88e4ff48de94ea8d5
Shayna Jamieson <jamieson.shayna@gmail.com>
2020-01-16 17:46:25 -0800

    Add home page view

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
 <?php
+/**
+ * @author Shayna Jamieson
+ * @version 1.0
+ * URL: http://sjamieson.greenriverdev.com/328/dating/index.php
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require("vendor/autoload.php");
+
+// Instantiate F3
+$f3 = Base::instance(); // invoke static
+
+// define a default route
+// when the user navigates to the route directory of the project
+// this is what they should see
+$f3->route('GET /', function() {
+    // create a new view object by instantiating the fat-free templating class
+    $view = new Template();
+
+    // on the object template we render the home page through this route
+    echo $view->render('views/home.html');
+});
+
+// fun F3
+$f3->run();
